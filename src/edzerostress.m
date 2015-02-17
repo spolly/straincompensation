3a455755dd01e8cab1ddf2762f39305a07086237
Stephen J. Polly <spolly@gmail.com>
2015-02-17 15:07:03 -0500

    Adding functions edzerostress and matthewsblakeslee for use in calculating maximum repeat units

---
+++ src/edzerostress.m
@@ -0,0 +1,10 @@
+function [a0] = edzerostress(A1, t1, a1, A2, t2, a2)
+%Funtion edzerostress returns the effective lattice constant of a...
+%strained superlattice based on given material parameters.
+%
+% Copyright (C) 2014--2015 Stephen J. Polly and Alex J. Grede
+% GPL v3, See LICENSE for details
+% This function is part of straincomp (https://nanohub.org/resources/straincomp)
+a0=(A1*t1*a1*a2^2 + A2*t2*a2*a1^2)/(A1*t1*a2^2 + A2*t2*a1^2);
+end
+
---
+++ src/matthewsblakeslee.m
@@ -0,0 +1,21 @@
+function [hc]=matthewsblakeslee(hi,a0,e0,nu,alpha,lambda,acc)
+%Funtion matthewsblakeslee returns the critical thickness
+%of a superlattice with given material parameters.
+%
+% Copyright (C) 2014--2015 Stephen J. Polly and Alex J. Grede
+% GPL v3, See LICENSE for details
+% This function is part of straincomp (https://nanohub.org/resources/straincomp)
+try
+    hc=(a0/sqrt(2))/(2 * pi * e0)...
+        *(1 - nu * cos(alpha)^2)/((1 + nu)*cos(lambda))...
+        *(log(hi/(a0/sqrt(2)))+1);
+    if hc <= 0
+        hc=matthewsblakeslee(hi+hi, a0, e0, nu, alpha, lambda, acc);
+    elseif abs(hc-hi)/hc > acc
+        hc=matthewsblakeslee(hc, a0, e0, nu, alpha, lambda, acc);
+    end
+catch
+   hc=0;
+end
+end
+
